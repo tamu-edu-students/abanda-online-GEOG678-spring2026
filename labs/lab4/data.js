3b6ee60124c2dd67cc393ffaf4821be06b0b38a3
abanda90 <abpy1012@tamu.edu>
2026-04-05 23:15:04 -0500

    Add Lab 4 dataset and working script

---
+++ labs/lab4/data.js
@@ -0,0 +1,11 @@
+var theJSON = {
+    data: [
+        { name: "DFW Terminal A", lat: 32.8998, lon: -97.0403 },
+        { name: "DFW Terminal B", lat: 32.8970, lon: -97.0370 },
+        { name: "DFW Terminal C", lat: 32.9025, lon: -97.0450 },
+        { name: "DFW Terminal D", lat: 32.8940, lon: -97.0420 },
+        { name: "DFW Terminal E", lat: 32.9000, lon: -97.0390 },
+        { name: "DFW Remote Parking", lat: 32.9050, lon: -97.0500 },
+        { name: "DFW Cargo Area", lat: 32.8900, lon: -97.0300 }
+    ]
+};
---
+++ labs/lab4/script.js
@@ -0,0 +1,122 @@
+function Run() {
+    // Most likely the assignment data is stored in theJSON.data
+    var data = theJSON.data;
+
+    var topLeftPoint = findTopLeft(data);
+    var topRightPoint = findTopRight(data);
+    var bottomLeftPoint = findBottomLeft(data);
+    var bottomRightPoint = findBottomRight(data);
+
+    var boundingBox = {
+        topLeft: topLeftPoint,
+        topRight: topRightPoint,
+        bottomLeft: bottomLeftPoint,
+        bottomRight: bottomRightPoint
+    };
+
+    console.log("Bounding box computed for abpy1012@gmail.com:");
+    console.log(boundingBox);
+}
+
+function getLat(point) {
+    if (point.lat !== undefined) return point.lat;
+    if (point.latitude !== undefined) return point.latitude;
+    if (point.y !== undefined) return point.y;
+    return null;
+}
+
+function getLon(point) {
+    if (point.lon !== undefined) return point.lon;
+    if (point.lng !== undefined) return point.lng;
+    if (point.longitude !== undefined) return point.longitude;
+    if (point.x !== undefined) return point.x;
+    return null;
+}
+
+function findTopLeft(data) {
+    var bestPoint = null;
+
+    data.forEach(function(point) {
+        var lat = getLat(point);
+        var lon = getLon(point);
+
+        if (bestPoint === null) {
+            bestPoint = point;
+        } else {
+            var bestLat = getLat(bestPoint);
+            var bestLon = getLon(bestPoint);
+
+            if (lat > bestLat || (lat === bestLat && lon < bestLon)) {
+                bestPoint = point;
+            }
+        }
+    });
+
+    return bestPoint;
+}
+
+function findTopRight(data) {
+    var bestPoint = null;
+
+    data.forEach(function(point) {
+        var lat = getLat(point);
+        var lon = getLon(point);
+
+        if (bestPoint === null) {
+            bestPoint = point;
+        } else {
+            var bestLat = getLat(bestPoint);
+            var bestLon = getLon(bestPoint);
+
+            if (lat > bestLat || (lat === bestLat && lon > bestLon)) {
+                bestPoint = point;
+            }
+        }
+    });
+
+    return bestPoint;
+}
+
+function findBottomLeft(data) {
+    var bestPoint = null;
+
+    data.forEach(function(point) {
+        var lat = getLat(point);
+        var lon = getLon(point);
+
+        if (bestPoint === null) {
+            bestPoint = point;
+        } else {
+            var bestLat = getLat(bestPoint);
+            var bestLon = getLon(bestPoint);
+
+            if (lat < bestLat || (lat === bestLat && lon < bestLon)) {
+                bestPoint = point;
+            }
+        }
+    });
+
+    return bestPoint;
+}
+
+function findBottomRight(data) {
+    var bestPoint = null;
+
+    data.forEach(function(point) {
+        var lat = getLat(point);
+        var lon = getLon(point);
+
+        if (bestPoint === null) {
+            bestPoint = point;
+        } else {
+            var bestLat = getLat(bestPoint);
+            var bestLon = getLon(bestPoint);
+
+            if (lat < bestLat || (lat === bestLat && lon > bestLon)) {
+                bestPoint = point;
+            }
+        }
+    });
+
+    return bestPoint;
+}
